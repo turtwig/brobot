cfcdd6774cc2a66961850c668937526f8dbf8bfc
MisterCow <internets@finalboss.org>
2010-07-15 15:11:22 +0100

    Revert "making brobot more pro"
    This reverts commit 55df542e0ba59b7b9e93e2d426f13cb78eb01a59.

--- src/helper.cpp
+++ src/helper.cpp
@@ -32,7 +32,6 @@ void ascii(Brobot* bot, const Args& args) {
 		std::getline(file, line);
 		bot->irc->privmsg(target, line);
 	}
-	bot->irc->privmsg(target, "(ps: internets is pro)");
 };
 
 extern int jpegmain(int argc, char **argv, char **envp);
