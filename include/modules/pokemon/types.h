fefc7e277e0fcbe6ed4048a21dbb3ed2fccd9e50
Pedro <pedro@bear.gov.br>
2010-07-16 03:22:03 -0300

    add STAB calculation, use NaN instead of -1 to return errors

--- include/modules/pokemon/types.h
+++ include/modules/pokemon/types.h
@@ -51,9 +51,19 @@ namespace pokemon {
 	}
 
 	float TypeModifier(const Type& a, const Type& def1, const Type& def2) {
-		if (def1 == none && def2 == none)
-			return -1.0;
-		return 1.0 * types_internal::t(a, def1) * types_internal::t(a, def2);
+		if (a == none || (def1 == none && def2 == none))
+			return NaN;
+		return types_internal::t(a, def1) * types_internal::t(a, def2);
+	};
+
+	inline float SameTypeBonus(const Type& a, const Type& t1, const Type& t2) {
+		if (a == none || (t1 == none && t2 == none))
+			return NaN;
+		if (t1 == a || t2 == a) {
+			return 1.5;
+		} else {
+			return 1.0;
+		}
 	};
 }
 
